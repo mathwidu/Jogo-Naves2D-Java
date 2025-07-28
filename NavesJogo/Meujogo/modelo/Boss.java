32ca681cc748d29891f1849d9b947ae8a3d8b1ba
mathwidu <matheuswmd@gmail.com>
2025-07-28 16:55:23 -0300

    .

---
+++ NavesJogo/Meujogo/modelo/Boss.java
@@ -0,0 +1,82 @@
+package Meujogo.modelo;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public class Boss {
+    private Image imagem;
+    private int x, y;
+    private int largura, altura;
+    private boolean isVisivel;
+    private int vida;
+
+    private static final int VELOCIDADE = 2;
+    private static final int VIDA_INICIAL = 5;
+
+    public Boss(int x, int y) {
+        this.x = x;
+        this.y = y;
+        isVisivel = true;
+        vida = VIDA_INICIAL;
+
+        load();
+    }
+
+    public void load() {
+        ImageIcon referencia = new ImageIcon(getClass().getResource("/res/boss.png"));
+        imagem = referencia.getImage();
+
+        this.largura = imagem.getWidth(null);
+        this.altura = imagem.getHeight(null);
+    }
+
+    public void update() {
+        this.x -= VELOCIDADE;
+
+        if (this.x + largura < 0) {
+            isVisivel = false;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, largura, altura);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisivel() {
+        return isVisivel;
+    }
+
+    public void setVisivel(boolean isVisivel) {
+        this.isVisivel = isVisivel;
+    }
+
+    public static int getVelocidade() {
+        return VELOCIDADE;
+    }
+
+    public Image getImagem() {
+        return imagem;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public static int getVidaInicial() {
+        return VIDA_INICIAL;
+    }
+}
